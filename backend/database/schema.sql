a6411e11c83e11b709cb20326dcf9316869a0d46
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-11-30 15:47:15 +0100

    création de la base des 2 tables sport et team

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,5 +2,10 @@ create table team (
   id int unsigned primary key auto_increment not null,
   name varchar(255) not null,
   country varchar(100) not null,
-  league varchar(100) not null,
+  league varchar(100) not null
+);
+
+create table sport (
+  id int unsigned primary key auto_increment not null,
+  name varchar(100) not null
 );
