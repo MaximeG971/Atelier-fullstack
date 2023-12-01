30fb8566710dfba584f6ea0f66c6705323c00d61
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-01 16:13:27 +0100

    modification du schema sql pour passer les sports sur 1 ligne et création d'une fonction teamGet

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,15 +1,15 @@
 create table team (
   id int unsigned primary key auto_increment not null,
   name varchar(255) not null,
-  country varchar(100) not null,
-  league varchar(100) not null,
-  logo varchar(250) not null,
+  country varchar(255) not null,
+  league varchar(255) not null,
+  logo varchar(255) not null,
   sport_id int not null
 );
 
 create table sport (
   id int unsigned primary key auto_increment not null,
-  name varchar(100) not null
+  name varchar(255) not null
 );
 
 INSERT INTO team (name, country, league, logo, sport_id) VALUES ('Paris Saint-Germain', 'France', 'Ligue 1', 'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/480px-Paris_Saint-Germain_Logo.svg.png', 1);
@@ -19,6 +19,5 @@ INSERT INTO team (name, country, league, logo, sport_id) VALUES ('Real Madrid', 
 INSERT INTO team (name, country, league, logo, sport_id) VALUES ('SIG Strasbourg', 'France', 'Pro A', 'https://upload.wikimedia.org/wikipedia/fr/thumb/b/b0/Logo_SIG_Strasbourg_-_2015.svg/1200px-Logo_SIG_Strasbourg_-_2015.svg.png', 2);
 INSERT INTO team (name, country, league, logo, sport_id) VALUES ('Stade Toulousain', 'France', 'Top 14', 'https://upload.wikimedia.org/wikipedia/fr/0/01/Logo_Stade_Toulousain_Rugby.svg', 3);
 
-INSERT INTO sport (name) VALUES ('Football');
-INSERT INTO sport (name) VALUES ('Basketball');
-INSERT INTO sport (name) VALUES ('Rugby');
+INSERT INTO sport (name) VALUES ('Football'), ('Basketball'), ('Rugby');
+
